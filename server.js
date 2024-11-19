a34b673ccd6ce79424b4941d1592b4c9eedfdffc
damialade <omoladedamilola1989@gmail.com>
2024-11-19 17:18:38 +0100

    initial project setup and commit to git account

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const stripe = require("stripe")(
 
 const app = express();
 
-const path = require('path');
+const path = require("path");
 
 const port = process.env.PORT || 8080;
 
-if(process.env.NODE_ENV=== 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build','index.html'))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 
 app.use(cors());
