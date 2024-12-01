5e96de6ed41bafdc9d96046b20f7419dda48c251
Damilola Aladeokin <106777877+damialade@users.noreply.github.com>
2024-12-01 02:15:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Checkout Endpoint
-app.post("/payment", async (req, res) => {
+app.post("/checkout", async (req, res) => {
   let { cart, token } = req.body;
   try {
     // Validate cart data
@@ -25,7 +25,7 @@ app.post("/payment", async (req, res) => {
       source: token.id,
     });
 
-    const payment = await stripe.charges.create({
+    const charge = await stripe.charges.create({
       amount: cart.TotalProductPrice * 100, // Convert to cents
       currency: "usd",
       customer: customer.id,
