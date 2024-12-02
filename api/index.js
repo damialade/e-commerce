dee9bafafc5606308f65ba921791103c72ef2b96
Damilola Aladeokin <106777877+damialade@users.noreply.github.com>
2024-12-02 16:57:41 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors({ origin: "https://e-commerce-tiannah.vercel.app" }));
 
 // Checkout Endpoint
-app.post("/checkout", async (req, res) => {
+app.post("/api/checkout", async (req, res) => {
     let error;
     let status;
     try {
