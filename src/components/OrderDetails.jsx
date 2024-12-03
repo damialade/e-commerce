5b917b16fd4f543b13a96f677d8c97611fdac02e
Damilola Aladeokin <106777877+damialade@users.noreply.github.com>
2024-12-03 16:18:54 +0100

    Update OrderDetails.jsx

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -49,8 +49,9 @@ const OrderDetails = () => {
         
         const itemsSnapshot = await fs
           .collection("Orders")
-          .doc(orderId)
+          .doc(uid)
           .collection("Items")
+          .doc(orderId)
           .get();
 
         if (itemsSnapshot.empty) {
